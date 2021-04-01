08d372c97b4aa91d8a16f86f57aaf9206bc42493
Tracy Ammann <tea1986@gmail.com>
2021-03-31 18:30:46 -0700

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,23 +4,41 @@ import csv
 
 filename = (r'C:\Users\Scott\iCloudDrive\Documents\Tracy\Boot Camp\Python-challege\PyBank\Resources\budget_data.csv')
 
-#Define Column names
-Column1 = str(Date[0])
-Column2 = int(Profits/Losses[1])
-
-
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
-    #read the header row first
-    csv_header = next(csvreader)
 
-    #count the number of total months
-    Total_months = Date.count('Date')
+    Date = []
+    Profit_Losses = []
+    change = []
+    i = 0
+    #j = 0
+    k = 0
+    for row in csvreader:
+        if i==0:
+            print(row)
+            Column1 = row[0]
+            Column2 = row[1]
+            i = i + 1
+            continue
+        print(row)
+        Date.append(row[0])
+        Profit_Losses.append(int(row[1]))
+        #j = j + 1
 
-    print(Total_months)
+    for k in range(len(Profit_Losses)-1):
+        change.append(Profit_Losses[k+1]-Profit_Losses[k])
 
-    #read each row of data after the header
-    #for row in csvreader:
+#print sum of (Profit_Losses)
+print(sum(Profit_Losses))
+Average = sum(change)/len(change)
+print(len(Profit_Losses))
+print(Average)
+print(max(change))
+print(min(change))
+
+# total number of months
+total_months = len(Date)
+print(total_months)
+
             
 
